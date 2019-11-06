8bf52b8436676255b1dbd202517be03b945ecc75
SuperPanPita <52289507+SuperPanPita@users.noreply.github.com>
2019-11-06 21:38:29 +0100

    Update altitude.py

--- src/mini_projet/scripts/altitude.py
+++ src/mini_projet/scripts/altitude.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import NavSatFix
+from std_msgs.msg import Int32
 
+
+# Callback subsciber au topic /bebop/fix pour publier l'altitude mesuree dans un topic /altitude
 def callback(data):
-    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    # altitude_str = "Hello, I mesure %s" %data.altitude
-    pub = rospy.Publisher('altitude_pub', NavSatFix,queue_size=10)
-    #pub.publish(data.altitude)
-    rate=rospy.Rate(10)
-    data=data.altitude
-    print data
+    dataAltitude=data.altitude
+    print dataAltitude
+    altiPub = rospy.Publisher('/altitude',Int32, NavSatFix,queue_size=10)    
+    altiPub.publish(dataAltitude) 
 
 
 def altitude(): 
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('altitude', anonymous=True)
     rospy.Subscriber('/bebop/fix', NavSatFix, callback)
-    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
